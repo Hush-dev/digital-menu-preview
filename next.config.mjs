7ccaf31296af36e626cdb5b158208c2a03766cd9
Aman Bansod <amansb69@gmail.com>
2026-05-30 17:23:48 +0530

    image changes

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
     remotePatterns: [
       { protocol: "https", hostname: "picsum.photos" },
       { protocol: "https", hostname: "**.supabase.co" },
+      { protocol: "https", hostname: "images.unsplash.com" },
     ],
   },
 };
